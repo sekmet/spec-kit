c7249c5566413a8774f5e6b7a89e583da22ca7d4
Carlos Guimaraes <sekmet@open4g.com>
2025-09-08 20:40:27 -0300

    chore: small updates

--- src/specify_cli/__init__.py
+++ src/specify_cli/__init__.py
@@ -390,7 +390,7 @@ def download_template_from_github(ai_assistant: str, download_dir: Path, *, verb
     """Download the latest template release from GitHub using HTTP requests.
     Returns (zip_path, metadata_dict)
     """
-    repo_owner = "github"
+    repo_owner = "sekmet"
     repo_name = "spec-kit"
     
     if verbose:
